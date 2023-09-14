be619529cd26b8391bf4f2447ace5151f909aa50
bassima_gh <ghba20wu@student.ju.se>
2023-09-14 16:13:14 +0200

    added project description to all the projects

---
+++ experiments/sound.js
@@ -0,0 +1,33 @@
+let player;
+let oscillator;
+let analyser;
+
+window.addEventListener("load", () => {
+  player = new Tone.Player("assets/heart-race.mp3");
+  oscillator = new Tone.Oscillator(440, "sine").toDestination();
+
+  analyser = new Tone.Analyser("fft", 4096);
+
+  oscillator.connect(analyser);
+  oscillator.toDestination();
+  player.connect(analyser);
+  player.toDestination();
+});
+
+window.addEventListener("click", () => {
+  // player.start();
+  oscillator.start();
+});
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+}
+
+function draw() {
+  background(255, 255, 255);
+  let value = analyser.getValue();
+  for (let i = 0; i < value.length; i++) {
+    let v = map(value[i], -100, 0, height, 0);
+    rect(i * 1, 0, 1, v); // waveform: * 100
+  }
+}
